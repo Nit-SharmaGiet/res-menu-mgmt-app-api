f37afd655fb73e45b068146b1a45a8b80af7ddf7
Nityananda Sharma <nitgithub@gmail.com>
2024-05-04 12:10:53 +0530

    added terraform using docker-compose-tf.yml and created std s3 backend with locking

---
+++ deploy/main.tf
@@ -0,0 +1,20 @@
+terraform {
+    backend "s3" {
+        bucket         = "res-menu-mgmt-app-api-tfstate"
+        key            = "res-menu-mgmt-app.tfstate"
+        region         = "us-east-1"
+        encrypt        = true
+        dynamodb_table = "res-menu-mgmt-app-api-tfstate-lock"
+    }
+
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.48.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
