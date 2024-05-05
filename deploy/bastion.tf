202de31eaa117967952e61121be7ba0c670e670e
Nityananda Sharma <nitgithub@gmail.com>
2024-05-06 00:24:37 +0530

    added bastion sg, and assigned this to db sg as well

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -30,7 +30,13 @@ resource "aws_instance" "bastion" {
       }
     )
   )
+  #add bastion sg:
+  vpc_security_group_ids = [
+    aws_security_group.bastion.id
+  ]
+
 }
+
 
 ####################################################################################
 #create a role by referring from instance-profile-policy.json
@@ -51,4 +57,44 @@ resource "aws_iam_role_policy_attachment" "bastion_attach_policy" {
 resource "aws_iam_instance_profile" "bastion" {
   name = "${local.prefix}-bastion-instance-profile"
   role = aws_iam_role.bastion.name
+}
+
+#aws security group for bastion
+resource "aws_security_group" "bastion" {
+  description = "Govern bastion inbound and outbound access"
+  name        = "${local.prefix}-bastion"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block,
+    ]
+  }
+
+  tags = local.common_tags
 }
